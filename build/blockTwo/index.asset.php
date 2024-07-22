df823aa313af194d221091826df8566ae9f4a73d
Mohamed Habib ALOUI <alouimohamedhabib@yahoo.com>
2024-07-22 22:58:21 +0200

    WIP

--- build/blockTwo/index.asset.php
+++ build/blockTwo/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6e53abc325b1c5d21579');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '46d0895618cfbcd73b49');
